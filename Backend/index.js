11623c88844c0bf43e2b90852a165ff690b878cb
pranay-parmarketing <pranay.badgujar@parmarketing.agency>
2025-01-21 16:02:22 +0530

    port no change

--- Backend/index.js
+++ Backend/index.js
@@ -124,5 +124,5 @@ app.get('/clear-cache', (req, res) => {
 
 
 // Server
-const PORT = process.env.PORT || 5000;
+const PORT = 8013;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
