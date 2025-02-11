ebcfb7bde4f2ea972bd5832a6ff44935239c2070
pranay-parmarketing <pranay.badgujar@parmarketing.agency>
2025-02-11 10:37:08 +0530

    file size update

--- Backend/index.js
+++ Backend/index.js
@@ -49,6 +49,8 @@ app.use('/api/saveToken',  saveToken);
 app.use('/api', utmRoutes);
 
 // 
+app.use(express.json({ limit: "200mb" }));
+app.use(express.urlencoded({ limit: "200mb", extended: true }));
 
 app.use('/api/leads',leadsRoute)
 app.use('/api/enrolle',enrolleRoute)
